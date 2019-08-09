4a9b3c5cef9dee9298a854c123806f93f0c0d896
JoyceLi1210 <52919089+JoyceLi1210@users.noreply.github.com>
2019-08-09 15:04:11 +0800

    socket

---
+++ socket/C#_server/socket/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace socket
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static Socket serverSocket = null;
+        static List<Socket> sockets = new List<Socket>();
+
+
+        public MainWindow()
+        {
+    
+            InitializeComponent();
+
+            
+
+        }
+        public void ListenClientConnect()
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                sockets.Add(clientSocket);
+                //为接受数据创建一个线程
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start(clientSocket);
+            }
+        }
+        public void ReceiveMessage(object clientSocket)
+        {
+            Socket connection = (Socket)clientSocket;
+            while (true)
+            {
+                try
+                {
+                    byte[] result = new byte[1024];
+                    //通过clientSocket接收数据  
+                    int receiveNumber = connection.Receive(result);
+                    //把接受的数据从字节类型转化为字符类型
+                    String recStr = Encoding.ASCII.GetString(result, 0, receiveNumber);
+
+
+                    //获取当前客户端的ip地址
+                    IPAddress clientIP = (connection.RemoteEndPoint as IPEndPoint).Address;
+                    //获取客户端端口
+                    int clientPort = (connection.RemoteEndPoint as IPEndPoint).Port;
+                    String sendStr = clientIP + ":" + clientPort.ToString() + "--->" + recStr;
+                    foreach (Socket socket in sockets)
+                    {
+                        socket.Send(Encoding.ASCII.GetBytes(sendStr));
+                    }
+                    //显示内容
+                    text1.Dispatcher.BeginInvoke(
+
+                            new Action(() => { text1.Text += "\r\n" + sendStr; }), null);
+
+                }
+                catch (Exception ex)
+                {
+
+                    connection.Shutdown(SocketShutdown.Both);
+                    connection.Close();
+                    break;
+                }
+            }
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse("192.168.43.155");
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(ip, 5050));  //绑定IP地址：端口  
+            serverSocket.Listen(10);    //设定最多10个排队连接请求             
+            //通过Clientsoket发送数据  
+            Thread myThread = new Thread(ListenClientConnect);
+            myThread.Start();
+
+            
+        }
+
+        /*
+        private void connect_click(object sender, RoutedEventArgs e)
+        {
+            System.Net.IPAddress theIPAddress;
+            //建立 IPAddress 物件(本機)
+            theIPAddress = System.Net.IPAddress.Parse("192.168.43.155");
+
+            //建立監聽物件
+            TcpListener myTcpListener = new TcpListener(theIPAddress, 5050);
+            //啟動監聽
+            myTcpListener.Start();
+            txt.AppendText("通訊埠 5050 等待用戶端連線...... !!");
+            // Console.WriteLine("通訊埠 5050 等待用戶端連線...... !!");
+            Socket mySocket = myTcpListener.AcceptSocket();
+            do
+            {
+                try
+                {
+                    //偵測是否有來自用戶端的連線要求，若是
+                    //用戶端請求連線成功，就會秀出訊息。
+                    if (mySocket.Connected)
+                    {
+                        int dataLength;
+                        txt.AppendText("連線成功 !!");
+                        byte[] myBufferBytes = new byte[10000];
+                        //取得用戶端寫入的資料
+                        dataLength = mySocket.Receive(myBufferBytes);
+
+                        txt.AppendText("接收到的資料長度" + dataLength.ToString() + "\n ");
+                        txt.AppendText("取出用戶端寫入網路資料流的資料內容 :");
+                        txt.AppendText(Encoding.ASCII.GetString(myBufferBytes, 0, dataLength) + "\n");
+                        // Console.WriteLine("按下 [任意鍵] 將資料回傳至用戶端 !!");
+                        // Console.ReadLine();
+                        //將接收到的資料回傳給用戶端
+                        mySocket.Send(myBufferBytes, myBufferBytes.Length, 0);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    mySocket.Close();
+                    break;
+                }
+
+            } while (true);*/
+    }
+    
+}
---
+++ socket/android_client/socket_2/app/src/main/java/com/example/socket_2/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.socket_2;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity{
+
+    private EditText edtname,edttext,ip,port;
+    private TextView textview1,ps,ps2;
+    private Button button1,connect;
+    String tmp;                // 暫存文字訊息
+    Socket clientSocket;
+    Thread t;
+    public static Handler mHandler = new Handler();
+
+    //ip.getText().toString()
+    //Integer.parseInt( port.getText().toString().replaceAll("[\\D]", ""))
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        edtname = (EditText)findViewById(R.id.edtname);
+        edttext = (EditText)findViewById(R.id.edttext);
+        ip = (EditText)findViewById(R.id.ip);
+        port = (EditText)findViewById(R.id.port);
+        button1 = (Button)findViewById(R.id.button1);
+        connect = (Button)findViewById(R.id.connect);
+        textview1 = (TextView)findViewById(R.id.textView1);
+        ps = (TextView)findViewById(R.id.ps);
+        ps2 = (TextView)findViewById(R.id.ps2);
+
+        button1.setOnClickListener(btnlistener);
+        connect.setOnClickListener(con);
+
+
+
+
+    }
+    private View.OnClickListener con = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            t = new Thread(readData);
+            t.start();
+            // 啟動執行緒
+            ps.setText("連線成功");
+        }
+    };
+
+    private View.OnClickListener btnlistener = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+
+            if(clientSocket.isConnected()){
+
+                BufferedWriter bw;
+                try {
+                    // 取得網路輸出串流
+                    bw = new BufferedWriter( new OutputStreamWriter(clientSocket.getOutputStream()));
+                    // 寫入訊息
+                    bw.write(edtname.getText()+":"+edttext.getText()+"\n");
+                    // 立即發送
+                    bw.flush();
+                } catch (IOException e) {
+                }
+                // 將文字方塊清空
+                edttext.setText("");
+                ps2.setText("傳送成功");
+            }
+        }
+    };
+
+    // 顯示更新訊息
+    private Runnable updateText = new Runnable() {
+        public void run() {
+            // 加入新訊息並換行
+            textview1.append(tmp + "\n");
+
+        }
+    };
+
+    // 取得網路資料
+    private Runnable readData = new Runnable() {
+        public void run() {
+            // server端的IP
+            InetAddress serverIp;
+            try {
+                // 以內定(本機電腦端)IP為Server端
+                serverIp = InetAddress.getByName("192.168.43.155");
+                int serverPort = 5050 ;
+                clientSocket = new Socket(serverIp, serverPort);
+
+                // 取得網路輸入串流
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        clientSocket.getInputStream()));
+
+                // 當連線後
+                while (clientSocket.isConnected()) {
+                    // 取得網路訊息
+                    tmp = br.readLine();
+
+                    // 如果不是空訊息則
+                    if(tmp!=null)
+                        // 顯示新的訊息
+                        mHandler.post(updateText);
+                }
+
+            } catch (IOException e) {
+            }
+        }
+    };
+
+}
